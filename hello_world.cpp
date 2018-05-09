084e6ceed22bc63b011556735ff774a5748ddb56
Shir Peled <shir@starkware.co>
2018-05-09 15:14:40 +0300

    clang format applied

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,13 +2,15 @@
 #include <string>
 
 std::string recursive_world(const unsigned u) {
-    if (u) {
-        return std::string("world ") + recursive_world(u - 1);
-    } else {
-        return std::string("world!\n");
-    }
+  if (u) {
+    return std::string("world ") + recursive_world(u - 1);
+  } else {
+    return std::string("world!\n");
+  }
 }
 
 int main() {
-    std::cout << "Hello " << recursive_world(9);
+  std::cout << "This is what's going on right now";
+  std::cout << "Hello " << recursive_world(9);
+  std::cout << "Yes";
 }
