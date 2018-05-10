f91931ae8a28c0ac7d8f8764b61b14a2aad4777d
Shir Peled <shir@starkware.co>
2018-05-10 07:22:29 +0300

    code review fix

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 std::string recursive_world(const unsigned u) {
-  if (u) {
+  if (u != 0) {
     return std::string("world ") + recursive_world(u - 1);
   } else {
     return std::string("world!\n");
